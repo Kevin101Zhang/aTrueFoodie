507b172d03dd6b23038f6879f1ae6787038ecdc1
JonFromNYC <82.Garcia@gmail.com>
2019-01-26 13:47:32 -0500

    changed schema to store one real address

--- schema.sql
+++ schema.sql
@@ -1,24 +1,16 @@
--- In Progress. USES MySQL.  
+-- In Progress. USES MySQL.
 
 use restaurants_db;
 
 create table ratings (
 id integer(10) AUTO_INCREMENT,
-yelpName varchar(80) NOT NULL, 	    -- Yelp Restaurant name
-longitude DECIMAL(10) NOT NULL,	    -- Yelp Longitude
-latitude DECIMAL(10) NOT NULL,	 	-- Yelp Latitude
-yelpRating DECIMAL(4) NOT NULL, 	-- Yelp Rating
-
-/* SECOND API */
-zomato_Name varchar(80) NOT NULL,	-- Zomato Name
-zomato_Rating DECIMAL(4) NOT NULL,	-- Zomato Rating
-zomato_latitude DECIMAL(10) NOT NULL, -- Zomato Latitude
-zomato_longitude DECIMAL(10) NOT NULL, -- Zomato Longitude
-
-/* THIRD API */
-api3Name varchar(80) NOT NULL,
-api3Rating DECIMAL(4) NOT NULL,
-api3_latitude DECIMAL(10) NOT NULL,
-api3_longitude DECIMAL(10) NOT NULL,
+yelpName varchar(80) NOT NULL,      -- Yelp Restaurant name
+yelpRating DECIMAL(4) NOT NULL,     -- Yelp Rating
+zomato_Name varchar(80) NOT NULL,   -- Zomato Name
+zomato_Rating DECIMAL(4) NOT NULL,  -- Zomato Rating
+api3Name varchar(80) NOT NULL,      -- Third Source Name
+api3Rating DECIMAL(4) NOT NULL,     -- Third Source Rating
+-- Hold Physical address for future searches
+physical_Location varchar(100) NOT NULL,
 PRIMARY KEY(id) -- Primary Key
 );
