62cefb17ab9347848c5a0d9618c850b5fc84ea02
Pinyin <kevin101zhang@gmail.com>
2019-02-02 14:10:16 -0500

    SEQ TABLE FAV FOOD

--- models/logging.js
+++ models/logging.js
@@ -1,4 +1,5 @@
 var bycrpt = require("bcrypt");
+//For Login
 module.exports = function (sequelize, DataTypes) {
     var signUpInfo = sequelize.define("signUpInfo", {
         username: DataTypes.STRING,
@@ -9,4 +10,15 @@ module.exports = function (sequelize, DataTypes) {
         return bycrpt.compareSync(password, this.password);
     };
     return signUpInfo;
+};
+
+//For Fav Rest
+module.exports = function (sequelize, DataTypes) {
+    var favorite_restaurants = sequelize.define("favorite_restaurants", {
+        id: DataTypes.INTEGER,
+        username: DataTypes.STRING,
+        yelp_id: DataTypes.STRING,
+    });
+
+    return favorite_restaurants;
 };
