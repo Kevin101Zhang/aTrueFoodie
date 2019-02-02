8a1d408222941b10fcce7a56ce373b9f3c258034
JonFromNYC <82.Garcia@gmail.com>
2019-02-02 13:15:10 -0500

    created two folders for path to production and test html

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -4,6 +4,8 @@ let db = require("../models");
 
 module.exports = function (app) { 
 
+    // PRODUCTION HTML FILES USE THESE FILES FOR PRODUCTION
+    
     // HOME PAGE
     app.get("/home", function (req,res) {
         // TODO: SAVE THE REQUEST TO DATABASE TO SEE WHAT DEVICE THE USER IS USING
@@ -28,6 +30,33 @@ module.exports = function (app) {
 
     // Our Story PAGE -- original
     app.get("/signup", function (req,res) {
-        res.sendFile(path.join(__dirname, "../public/signup.html"));
+        res.sendFile(path.join(__dirname, "../uat_html/public/signup.html"));
     });
+
+    // PROTO-TYPE - User Acceptence Testing
+    // USE THESE ROUTES BELOW FOR TESTING
+        // UAT HOME PAGE
+        app.get("/uat/home", function (req,res) {
+            res.sendFile(path.join(__dirname, "../uat_html/public/home.html"));
+        });
+    
+        // UAT CONTACT PAGE
+        app.get("/uat/contact", function (req,res) {
+            res.sendFile(path.join(__dirname, "../uat_html/public/contact.html"));
+        });
+    
+        // UAT index PAGE -- original
+        app.get("/uat/index", function (req,res) {
+            res.sendFile(path.join(__dirname, "../uat_html/public/index.html"));
+        });
+    
+        // UAT Our Story PAGE -- original
+        app.get("/uat/ourstory", function (req,res) {
+            res.sendFile(path.join(__dirname, "../uat_html/public/ourstory.html"));
+        });
+    
+        // UAT Our Story PAGE -- original
+        app.get("/uat/signup", function (req,res) {
+            res.sendFile(path.join(__dirname, "../uat_html/public/signup.html"));
+        });
 };
