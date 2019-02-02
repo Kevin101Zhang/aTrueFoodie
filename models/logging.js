8d0b347619083c8ba31674e309073354e6f51e63
Pinyin <kevin101zhang@gmail.com>
2019-02-02 14:17:12 -0500

    SQLTABLE

--- models/logging.js
+++ models/logging.js
@@ -1,6 +1,7 @@
 var bycrpt = require("bcrypt");
 //For Login
 module.exports = function (sequelize, DataTypes) {
+
     var signUpInfo = sequelize.define("signUpInfo", {
         username: DataTypes.STRING,
         password: DataTypes.STRING
@@ -9,16 +10,8 @@ module.exports = function (sequelize, DataTypes) {
     signUpInfo.prototype.validatePassword = function (password) {
         return bycrpt.compareSync(password, this.password);
     };
+
     return signUpInfo;
 };
 
 //For Fav Rest
-module.exports = function (sequelize, DataTypes) {
-    var favorite_restaurants = sequelize.define("favorite_restaurants", {
-        id: DataTypes.INTEGER,
-        username: DataTypes.STRING,
-        yelp_id: DataTypes.STRING,
-    });
-
-    return favorite_restaurants;
-};
