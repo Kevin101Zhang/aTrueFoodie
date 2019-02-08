e28caf4e8556242ece591939c8ac99a51d80c29e
Liberatone <musictus@gmail.com>
2019-02-07 20:29:12 -0500

    data changes

--- public/js/yelp.js
+++ public/js/yelp.js
@@ -31,14 +31,22 @@ function getLocation() {
             restaurantImage.attr('alt', "Doctor Image");
             restaurantImage.attr('style', "display: block; margin-left: 0px; margin-right: auto; width: 20%");
 
-        var restaurantInfo = ("<p id='restaurant-info'>" + res.yelpData.name + "<br>" + res.yelpData.location + "<br>" + res.yelpData.phone + "</p><br>")
+        var restaurantInfo = ("<span id='restaurant-info'>" + res.yelpData.name + "<br>" + res.yelpData.location + "<br>" + res.yelpData.phone + "</span><br><br>")
 
         // yelp and google ratings here
         var yelpGoogleData = ("<span id='yelp-google-rating'>" + "Yelp Rating: " + res.yelpData.rating + "</span><br>" + "<span>" + "Google Rating: " + res.googleData.rating + "</span><br>");
 
         // our true rating here
-        var trueDataRating = ("<span id='true-rating'>" + "Average Rating: " + res.trueReview.trueRating + "<br>" + "Total Review Count: " + res.trueReview.total_review_count + "</span><br>");
+        var trueDataRating = ("<span id='true-rating'>" + "True Foodie's Aggregate Rating: " + res.trueReview.trueRating + "<br>" + "Total Review Count: " + res.trueReview.total_review_count + "</span><br>");
 
+        // NYC health department grade here
+        if (res.food_rating === undefined) {
+            var healthGrade = ("<span id='health-grade'>" + "NYC Health Department Grade: No rating at this time" + "</span><br><br><br>");
+        } else {
+            var healthGrade = ("<span id='health-grade'>" + "NYC Health Department Grade: " + res.food_rating.grade + "</span><br><br><br>");
+        }
+
+        $("#true-result").prepend(healthGrade);
         $("#true-result").prepend(trueDataRating);
         $("#true-result").prepend(yelpGoogleData);
         $("#true-result").prepend(restaurantInfo);
